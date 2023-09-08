09bf030abadc40434bb58ee25446b40145ace5b5
YarikRy <yar.rybkin@gmail.com>
2023-09-07 21:17:13 -0500

    messed with the dealCards method

--- week6.js
+++ week6.js
@@ -1,23 +1,26 @@
 class Game {
     constructor(name){
         this.name = name
-        this.deck = []
+        this.deck = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10,'Jack', 'Queen', 'King',"Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10,'Jack', 'Queen', 'King',"Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10,'Jack', 'Queen', 'King',"Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10,'Jack', 'Queen', 'King']
+        this.cards1 = []
+        this.cards2 = []
     }
 
-    createDeck(){
-        for(let i = 0; i < 4; i++){
-            this.deck.push("Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10,'Jack', 'Queen', 'King')
-        }
-    }
+    // createDeck(){
+    //     for(let i = 0; i < 4; i++){
+    //         this.deck.push("Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10,'Jack', 'Queen', 'King')
+    //     }
+    // }
 
     dealCards(){
-        const randomCards = []
-        for(let i = 0; i < 26; i++){
-            let random = Math.floor(Math.random() * this.deck.length)
-            let randomCard = this.deck[random]
-            randomCards.push(randomCard)
-            this.deck -= randomCard
+        for (let i = 0; i < 26; i++){
+            const random = Math.floor(Math.random() * this.deck.length)
+            this.cards1.push(this.deck[random])
+            //this.deck.splice(this.deck[random], 1)
+            this.deck.splice(this.cards1[i], 1)
+            console.log(this.deck)
         }
+        
     }
 }
 class Player {
@@ -55,8 +58,6 @@ class Player {
 // console.log(deck1)
 let player1 = new Player('p1')
 let war = new Game('War')
-war.createDeck()
-console.log(war.deck)
 war.dealCards()
-console.log(war.deck)
+console.log(war.cards1)
 
